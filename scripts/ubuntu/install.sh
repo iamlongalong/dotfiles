d8bb31c1e934316904b1387c55ce6bbd8708e4a9
huhailong.andy <huhailong.andy@bytedance.com>
2025-04-18 18:49:35 +0800

    using case for bash legency

--- scripts/ubuntu/install.sh
+++ scripts/ubuntu/install.sh
@@ -524,9 +524,8 @@ install_docker() {
         
         # 清理可能存在的旧安装
         log "INFO" "Removing any existing Docker installations..."
-        sudo apt remove --purge -y docker.io docker-engine docker containerd runc || true
-        sudo rm -rf /var/lib/docker /etc/docker
-        
+        for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
         # 安装 Docker
         log "INFO" "Installing Docker using curl..."
         # if ! sudo apt install -y docker.io containerd docker-compose; then
@@ -534,7 +533,21 @@ install_docker() {
         #     return 1
         # fi
 
-        curl -sSL https://get.docker.com/ | CHANNEL=stable sh
+        # Add Docker's official GPG key:
+        sudo apt-get update
+        sudo apt-get install -y ca-certificates curl
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+        # Add the repository to Apt sources:
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get update
+
+        sudo apt-get install -y docker-ce docker-ce-cli docker.io containerd containerd.io docker-buildx-plugin docker-compose-plugin
         
         # 启动 Docker 服务前检查系统状态
         log "INFO" "Checking system status before starting Docker..."
